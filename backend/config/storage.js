1529aedc8a8413722879248b219e66274c74a812
SubhamUpadhyay <upadhyaysubham325@gmail.com>
2025-10-24 09:59:47 +0530

    storage configured

---
+++ backend/config/storage.js
@@ -0,0 +1,44 @@
+// config/storage.js
+const multer = require('multer');
+const path = require('path');
+const fs = require('fs');
+
+const uploadDir = path.join(__dirname, '../uploads');
+
+if (!fs.existsSync(uploadDir)) {
+  fs.mkdirSync(uploadDir, { recursive: true });
+}
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, uploadDir);
+  },
+  filename: (req, file, cb) => {
+    const uniqueName = Date.now() + '-' + Math.round(Math.random() * 1e9) + path.extname(file.originalname);
+    cb(null, uniqueName);
+  }
+});
+
+const fileFilter = (req, file, cb) => {
+  const allowedMimes = {
+    image: /image/,
+    video: /video/
+  };
+  
+  const type = req.baseUrl.split('/')[3];
+  const allowedMime = allowedMimes[type];
+  
+  if (allowedMime && allowedMime.test(file.mimetype)) {
+    cb(null, true);
+  } else {
+    cb(new Error(`Invalid file type`));
+  }
+};
+
+const upload = multer({
+  storage,
+  limits: { fileSize: 100 * 1024 * 1024 },
+  fileFilter
+});
+
+module.exports = upload;
